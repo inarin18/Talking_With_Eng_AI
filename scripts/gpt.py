f8c9773cb43936e265196fb70e1d118a4530d02d
Hinata <hi710i7oka@outlook.jp>
2024-01-04 00:28:36 +0900

    userのメッセージが反映されていなかったバグを修正．

--- scripts/gpt.py
+++ scripts/gpt.py
@@ -31,13 +31,18 @@ def Chat():
     
     # gpt に対する指示
     instruction = """
-        You are the native person in English.
-        You are going to talk with a person who is not good at English.
-        All you have to do is to talk with him/her.
-        If you find that 'user' made a mistake, You should inform him/her of the mistake 
-        and then correct it.
-        You should comply with the following rules.
-            1. You should not use any words other than English.
+    You are the native person in English.
+    You are going to talk with a person who is not good at English.
+    All you have to do is to conversation with him/her.
+    You should comply with the following rules.
+        1. If you find that 'user' made a mistake, You should inform him/her of the mistake and then correct it.
+            ex 1) user : Hello I am a student.
+            then you should respond on context.
+            ex 2) user : Hello She am a student.
+            then you should insist that 'she' is wrong and correct it like this.
+            You : Your sentence is wrong. You should say 'I' instead of 'she'.
+        2. You should talk with user as if you are a native person in English.
+        3. Your resopnse should be on a context.
     """
     messages.append({"role": "system", "content": instruction})
     
@@ -45,10 +50,12 @@ def Chat():
     
     while True :
             
-        instruction = input(">>> ")
+        user_content = input(">>> ")
         
-        if instruction == "exit" :
+        if user_content == "exit" :
             break
+        
+        messages.append({"role": "user", "content": user_content})
         
         chat_completion = client.chat.completions.create(
             messages=messages, model="gpt-3.5-turbo",
