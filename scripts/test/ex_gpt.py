c8f5fed744a49aee53bba3e1ec9b44d236b5daca
Hinata <hi710i7oka@outlook.jp>
2024-01-03 23:35:00 +0900

    openai api μの使用例を追加

--- scripts/test/ex_gpt.py
+++ scripts/test/ex_gpt.py
@@ -1,29 +1,50 @@
 # 
 # OPENAI-Reference
 # https://platform.openai.com/docs/api-reference
+# https://github.com/openai/openai-python
 #
 
 import os
-import openai
+from openai import OpenAI
 
 # for personal
-openai.organization = "org-NCk1NUFa2Aw0WAB0lUKsbE9u"
-# redefine after importing __init__.py of openai
-openai.api_key = os.getenv("OPENAI_API_KEY")
+# openai.organization = "org-NCk1NUFa2Aw0WAB0lUKsbE9u"
 
-completion = openai.ChatCompletion.create(
-    model = "gpt-3.5-turbo",
-    messages = [
-        {"role": "user", "content": "Hello!"}
-    ]
+# API キーを用いてクライアントを初期化
+client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY")
 )
 
-print(completion.choices[0].message.content)
+""" completion """
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Say this is a test",
+        }
+    ],
+    model="gpt-3.5-turbo",
+)
 
-res = openai.Image.create(
-    prompt = "A cute baby sea otter",
-    n = 2,
-    size = "1024x1024"
+print(chat_completion.choices[0].message.content)
+
+""" stream """ 
+stream = client.chat.completions.create(
+    model="gpt-4",
+    messages=[{"role": "user", "content": "Say this is a test"}],
+    stream=True,
+)
+for chunk in stream:
+    if chunk.choices[0].delta.content is not None:
+        print(chunk.choices[0].delta.content, end="")
+
+
+""" image  """
+res = client.images.generate(
+    model="dall-e-3",
+    prompt="A cute baby sea otter",
+    n=1,
+    size="1024x1024"
 )
 
 print(res.data)
