a1ca387c0797361ec6dc0584d1ff7959800240a0
Hinata <hi710i7oka@outlook.jp>
2024-01-06 20:51:41 +0900

    各種関数をインポート

---
+++ main.py
@@ -0,0 +1,30 @@
+""" 
+Date : 2023-12-30
+
+Author : Hinata Inaoka
+
+Description : 
+    This is a main file of this project.
+
+"""
+
+import os
+
+from openai import OpenAI
+
+from scripts.s2t import realtime_textise
+from scripts.gpt import Chat 
+from scripts.t2s import t2s
+
+
+client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY")
+)
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main() 
