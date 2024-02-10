71c814759b2e7c58c0891a90a577cff4f3471c70
Hinata <69310436+inarin18@users.noreply.github.com>
2024-02-11 04:39:19 +0900

    concurrent を用いた非同期処理を削除

--- scripts/s2t.py
+++ scripts/s2t.py
@@ -6,7 +6,6 @@ import sys
 from pathlib import Path
 
 import speech_recognition as sr
-import concurrent.futures
 import pyaudio
 
 
@@ -18,67 +17,47 @@ __ALL__ = [
 
 # 以下 import 時に実行されるコード
 
-# 非同期処理を行うための実行者を生成
-global executor
-executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
+
+global stream_data 
+
 
 # PyAudio に用いるパラメータ
 FORMAT              = pyaudio.paInt16
 SAMPLE_RATE         = 44100    # サンプリングレート
 CHANNELS            = 1        # モノラルかバイラルか
 INPUT_DEVICE_INDEX  = 0        # マイクのチャンネル
-CALL_BACK_FREQUENCY = 10       # コールバック呼び出しの周期[sec]
+CALL_BACK_FREQUENCY = 3        # コールバック呼び出しの周期[sec]
 
 # アウトプットファイルのパス
 OUTPUT_TXT_FILE = Path(__file__).parent.parent / "data" / "user_speech.txt"
 
 
-""" デバイス上でのオーディオ系の機器情報を表示する """
-def look_for_audio_input():
-    
-    pa = pyaudio.PyAudio()
-
-    for i in range(pa.get_device_count()):
-        print(pa.get_device_info_by_index(i))
-        print()
-
-    pa.terminate()
-
-
-""" コールバック関数の定義 """
+# コールバック関数の定義
 def callback(in_data, frame_count, time_info, status):
 
-    # speech_recognitionオブジェクトを毎回作成するのではなく、使いまわすために、グローバル変数で定義しておく
-    global sprec 
+    stream_data.append(in_data)
 
-    try:
+    return (None, pyaudio.paContinue)
+
+
+def show_speech(in_data):
+    
+    try :
         audiodata  = sr.AudioData(in_data, SAMPLE_RATE, 2)
         sprec_text = sprec.recognize_google(audiodata, language='eng')
+        print("Your Speech : " + sprec_text)
         
-        with open(OUTPUT_TXT_FILE, 'a') as f: #ファイルの末尾に追記していく
-            f.write("\n" + sprec_text)
-    
-    except sr.UnknownValueError:
-        pass
+    except sr.UnknownValueError as e:
+        print(e)
     
     except sr.RequestError as e:
-        pass
+        print(e)
     
     finally:
-        return (None, pyaudio.paContinue)
-    
-
-# コールバック関数を非同期で実行するためのラッパー関数
-def async_callback(in_data, frame_count, time_info, status):
-    
-    global executor
-    
-    future = executor.submit(callback, in_data, frame_count, time_info, status)
-    
-    return future.result()
+        pass
 
 
-""" リアルタイムで音声を文字起こしする """
+# リアルタイムで音声を文字起こしする
 def realtime_textise():
 
     global sprec # speech_recognitionオブジェクトを毎回作成するのではなく、使いまわすために、グローバル変数で定義しておく
@@ -97,13 +76,22 @@ def realtime_textise():
         input_device_index = INPUT_DEVICE_INDEX,
         input              = True, 
         frames_per_buffer  = SAMPLE_RATE*CALL_BACK_FREQUENCY, # CALL_BACK_FREQUENCY 秒周期でコールバック
-        stream_callback    = async_callback
+        stream_callback    = None
     )
     
     stream.start_stream()
     
-    while stream.is_active() :
+    i = 0
+    while stream.is_active() and i < 1000:
+        
         time.sleep(0.01)
+        i += 1
+        print("\r" + str(i), end="")
+        
+    sprec = sr.Recognizer() 
+
+    for in_data in stream_data:
+        show_speech(in_data)
     
     stream.stop_stream()
     stream.close()
