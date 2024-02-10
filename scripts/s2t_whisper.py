8fb1ffb115b8546e530a480108b57798e08a559e
Hinata <69310436+inarin18@users.noreply.github.com>
2024-02-11 06:02:08 +0900

    whisper を用いた s2t を実装

---
+++ scripts/s2t_whisper.py
@@ -0,0 +1,67 @@
+
+import os
+import wave
+import pyaudio
+
+from pathlib import Path
+from openai import OpenAI
+
+from modules.audio_parameter import (
+    FORMAT             ,
+    SAMPLE_RATE        ,
+    CHANNELS           ,
+    INPUT_DEVICE_INDEX ,
+    CALL_BACK_FREQUENCY,
+)
+
+client = OpenAI(
+    api_key=os.environ["OPENAI_API_KEY"]
+)
+
+# ファイルのパス
+OUTPUT_TXT_FILE = Path(__file__).parent.parent / "data" / "user_speech.txt"
+AUDIO_FILE_PATH = Path(__file__).parent.parent / "sounds" / "output.wav"
+
+# 音声を文字起こしする
+def speech_2_text():
+
+    CHUNK = 1024
+    RECORD_SECONDS = 5
+
+    with wave.open('sounds/output.wav', 'wb') as wf:
+        
+        p = pyaudio.PyAudio()
+        wf.setnchannels(CHANNELS)
+        wf.setsampwidth(p.get_sample_size(FORMAT))
+        wf.setframerate(SAMPLE_RATE)
+
+        stream = p.open(format=FORMAT, channels=CHANNELS, rate=SAMPLE_RATE, input=True)
+
+        print('Recording...')
+        for _ in range(0, SAMPLE_RATE // CHUNK * RECORD_SECONDS):
+            
+            in_data = stream.read(CHUNK)
+            
+            wf.writeframes(in_data)
+                
+        print('Done')
+
+        stream.close()
+        p.terminate()
+    
+    audio_file = open(AUDIO_FILE_PATH, "rb")
+    transcript = client.audio.transcriptions.create(
+        model = "whisper-1", 
+        file  = audio_file,
+        response_format="text"
+    )
+
+    print(transcript)
+
+
+def main():
+    speech_2_text()
+
+
+if __name__ == '__main__':
+    main()
