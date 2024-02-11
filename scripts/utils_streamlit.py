75ea5751f6df4af042d7fd1e7b161cc350c1722c
Hinata <69310436+inarin18@users.noreply.github.com>
2024-02-11 12:19:33 +0900

    streamlit の初期化時処理にシステムへのプロンプトを追加

--- scripts/utils_streamlit.py
+++ scripts/utils_streamlit.py
@@ -1,15 +1,25 @@
 import streamlit as st
 
+from .prompt import INSTRUCTION
+
 
 def init_streamlit():
     
     if "conversation_history" not in st.session_state:
         st.session_state.conversation_history = []
         
+        # gpt への指示を履歴に追加
+        st.session_state.conversation_history.append(
+            {
+                "role": "system", 
+                "content": INSTRUCTION
+            }
+        )
+        
 
 # 会話履歴をチャット形式で表示
 def show_conversation():
         
-        for message in st.session_state.conversation_history:
+        for message in st.session_state.conversation_history[1:]:
             with st.chat_message(message["role"]):
                 st.write(message["content"])
