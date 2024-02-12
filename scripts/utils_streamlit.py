cf4c8743300320610e5b38923d0dae84e39c258a
Hinata <69310436+inarin18@users.noreply.github.com>
2024-02-12 11:27:23 +0900

    callback関数に追加変更

--- scripts/utils_streamlit.py
+++ scripts/utils_streamlit.py
@@ -3,14 +3,6 @@ import streamlit as st
 from .prompt import INSTRUCTION
 
 
-# curr_state :
-# "init"
-# "waiting_for_user_input"
-# "in_speech_2_text"
-# "waiting_for_gpt_response"
-# "waiting_for_sound_response"
-
-
 def init_streamlit():
     
     if "conversation_history" not in st.session_state:
@@ -27,30 +19,35 @@ def init_streamlit():
     if "mic_disabled" not in st.session_state:
         st.session_state.mic_disabled = False
         
-    if "is_skip_record" not in st.session_state:
-        st.session_state.is_skip_record = False
+    if "is_still_recording" not in st.session_state:
+        st.session_state.is_still_recording = False
         
-    if "prompt" not in st.session_state:
-        st.session_state.prompt = None
+    if "is_skip_recording" not in st.session_state:
+        st.session_state.is_skip_recording = False
+        
+    if "is_stop_recording" not in st.session_state:
+        st.session_state.is_stop_recording = False
         
     if "user_sentence" not in st.session_state:
         st.session_state.user_sentence = None
         
     if "gpt_response" not in st.session_state:
         st.session_state.gpt_response = None
-        
-        
-
-# 会話履歴をチャット形式で表示
-def show_conversation():
-    
-    # システムの発言は表示しない
-    for message in st.session_state.conversation_history[1:]:
-        with st.chat_message(message["role"]):
-            st.write(message["content"])
 
 
 # コールバック関数として用いることが多い
 def change_mic_state_to_disabled(disabled :bool = True):
     
     st.session_state.mic_disabled = disabled
+    
+
+def change_recording_state_to_true():
+    
+    # 録音終了時に録音スキップフラグを立てる
+    st.session_state.is_skip_recording  = True
+    
+    # 録音停止ボタンを制御するためのフラグを立てる
+    st.session_state.is_stop_recording  = True
+    
+    # 録音中のフラグを立てる
+    st.session_state.is_still_recording = True
