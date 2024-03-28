39a8f2c776dd1c4c058734b302825c87f9b4a0ef
Hinata <hi710i7oka@outlook.jp>
2024-03-28 19:04:28 +0900

    使用している OS が macOS の際にバッファサイズが小さいとオーバーフローする問題を CHUNK を 大きくすることで解決

--- scripts/modules/audio_parameter.py
+++ scripts/modules/audio_parameter.py
@@ -1,7 +1,13 @@
+import os
 import pyaudio
 
 # PyAudio に用いるパラメータ
-CHUNK               = 1024
+
+# os が macOS の際はバッファリングサイズが小さいと
+# OSError: [Errno -9981] Input overflowed が発生するため
+# バッファリングサイズを大きくしている
+CHUNK               = 1024 if os.name == "nt" else 1024 * 4 
+
 FORMAT              = pyaudio.paInt16
 SAMPLE_RATE         = 44100    # サンプリングレート
 CHANNELS            = 1        # モノラルかバイラルか
