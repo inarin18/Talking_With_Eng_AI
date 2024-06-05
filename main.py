68bc6e4cdec01e8f5a377fbc137ce24a99771216
Hinata <hi710i7oka@outlook.jp>
2024-06-05 16:28:59 +0900

    API の認証機能を追加

--- main.py
+++ main.py
@@ -13,16 +13,12 @@ import queue
 import streamlit as st
 
 from openai import OpenAI
+from openai import AuthenticationError
 
 
 from scripts import utils_streamlit
 from scripts import widget_handler
 from scripts import t2s
-
-
-client = OpenAI(
-    api_key=os.environ.get("OPENAI_API_KEY")
-)
 
 
 def main():
@@ -32,6 +28,14 @@ def main():
     
     st.title("English Conversation with GPT")
     
+    if os.environ.get("OPENAI_API_KEY") is None :
+        st.error("Please set your OpenAI API key in the environment variable.")
+        st.stop()
+        
+    client = OpenAI(
+        api_key=os.environ.get("OPENAI_API_KEY")
+    )
+    
     # チャット形式で会話履歴の表示
     widget_handler.show_conversation()
     
@@ -39,20 +43,22 @@ def main():
     widget_handler.locate_input_widget()
 
     # ユーザの入力処理
-    widget_handler.handle_user_input()
+    widget_handler.handle_user_input(client)
     
     # ユーザの入力を処理できていれば，gpt の回答を表示
     if st.session_state.user_sentence is not None:
         
         widget_handler.handle_gpt_response(
+            client    = client,
             gpt_model = "gpt-3.5-turbo-0125"
         )
         
         # gpt の回答を音声化して再生
         t2s.text_2_speech(
-            text  = st.session_state.gpt_response,
-            model = "tts-1",
-            voice = "alloy"
+            client = client,
+            text   = st.session_state.gpt_response,
+            model  = "tts-1",
+            voice  = "alloy"
         )
         
         # ユーザの入力を初期化
@@ -65,4 +71,10 @@ def main():
 
 
 if __name__ == "__main__":
-    main() 
+    
+    try:
+        main()
+        
+    except AuthenticationError :
+        st.error("Please set your OpenAI API key correctly in the environment variable.")
+        st.stop()
