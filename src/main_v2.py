289156daf89393130c4f8e6e8a53debf9918d598
Hinata <69310436+inarin18@users.noreply.github.com>
2025-02-25 03:52:30 +0900

    add: realtime api script and exmaples

---
+++ src/main_v2.py
@@ -0,0 +1,32 @@
+import asyncio
+import os
+from openai import AsyncOpenAI
+
+async def main():
+    client = AsyncOpenAI(
+        api_key=os.environ.get("OPENAI_API_KEY")
+    )
+
+    async with client.beta.realtime.connect(model="gpt-4o-realtime-preview") as connection:
+        await connection.session.update(session={'modalities': ['text']})
+
+        await connection.conversation.item.create(
+            item={
+                "type": "message",
+                "role": "user",
+                "content": [{"type": "input_text", "text": "Say hello!"}],
+            }
+        )
+        await connection.response.create()
+
+        async for event in connection:
+            if event.type == 'response.text.delta':
+                print(event.delta, flush=True, end="")
+
+            elif event.type == 'response.text.done':
+                print()
+
+            elif event.type == "response.done":
+                break
+
+asyncio.run(main())
